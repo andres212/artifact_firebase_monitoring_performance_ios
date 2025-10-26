ebfed7b1a05212909402c2bfa189eb3268007d8d
Nelson Andres Sepulveda Vega <nsepulve@nequi.com>
2025-10-26 10:52:56 -0500

    Initial Commit

---
+++ MyAppMonitoringPerformance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyAppMonitoringPerformance
+//
+//  Created by Nelson Sepulveda on 26/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyAppMonitoringPerformance/MyAppMonitoringPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppMonitoringPerformanceApp.swift
+//  MyAppMonitoringPerformance
+//
+//  Created by Nelson Sepulveda on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAppMonitoringPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
