2efe347e6b4dff16b288f104ae0a9d8f5ca569ab
Nelson Andres Sepulveda Vega <nsepulve@nequi.com>
2025-10-30 00:49:33 -0500

    feat(firebase): integrate sdk firebase & firebas perfomance

--- MyAppMonitoringPerformance/MyAppMonitoringPerformanceApp.swift
+++ MyAppMonitoringPerformance/MyAppMonitoringPerformanceApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebasePerformance
 
 @main
 struct MyAppMonitoringPerformanceApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        if let options = FirebaseApp.app()?.options {
+          print("Firebase configurado: \(options.projectID ?? "Sin ID")")
+        }
+        
+        let initTrace = Performance.startTrace(name: "launch_app_ios_monitoring_performance")
+        initTrace?.incrementMetric("app_count", by: 1)
+        initTrace?.stop()
+        print("Evento: launch_app_ios_monitoring_performance")
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
